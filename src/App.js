8bbf114e4694fd5f02472911229e866cc489bb30
gabibble <87248000+gabibble@users.noreply.github.com>
2022-02-13 11:30:14 -0800

    footer component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
-      <p>
-        Open-source{" "}
-        <a
-          href="https://github.com/gabibble/my-react-weather-app.git"
-          target="_blank"
-        >
-          code
-        </a>{" "}
-        by Julia Giebultowicz
-      </p>{" "}
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <br />
+      <small>
+        Open-source{" "}
+        <a
+          href="https://github.com/gabibble/my-react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.juliagiebultowicz.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Julia Giebultowicz
+        </a>
+      </small>
+    </div>
+  );
+}
